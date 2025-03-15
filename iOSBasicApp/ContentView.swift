bb0639a82e053cf7860b80105ac4672258413e65
Pramuk U <pramuk.u1@viatris.com>
2025-03-15 11:06:11 +0530

    Initial commit

---
+++ iOSBasicApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSBasicApp
+//
+//  Created by Pramuk.u on 15/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSBasicApp/iOSBasicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSBasicAppApp.swift
+//  iOSBasicApp
+//
+//  Created by Pramuk.u on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSBasicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
